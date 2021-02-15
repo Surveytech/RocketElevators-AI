0c230fe7c97c85621d87e159f8926aa2778d17a7
CoteTommy <nordik.tommy@gmail.com>
2021-02-15 12:20:37 -0500

    Initial commit v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get "/articles", to: "articles#index"
+
+  get "/", to: "main#index"
+  get "/quote", to: "main#quote"
+  get "/residential", to: "main#residential"
+  get "/commercial", to: "main#commercial"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
