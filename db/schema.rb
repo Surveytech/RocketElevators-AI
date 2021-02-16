f741295c7539afa676730767c9a064ac0881b9c2
CoteTommy <nordik.tommy@gmail.com>
2021-02-15 21:56:31 -0500

    added name, surname to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_013150) do
+ActiveRecord::Schema.define(version: 2021_02_16_020019) do
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2021_02_16_013150) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
+    t.string "title"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# users.create!(name: 'Nicolas', surname: 'Genest', title: 'CEO', email: 'nicolas.genest@codeboxx.biz')
+# users.create!(name: 'Nadya', surname: 'Fortier', title: 'Director', email: 'nadya.fortier@codeboxx.biz')
+# users.create!(name: 'Martin', surname: 'Chantal', title: 'Director Assistant', email: 'martin.chantal@codeboxx.biz')
+# users.create!(name: 'Mathieu', surname: 'Houde', title: 'Captain', email: 'mathieu.houde@codeboxx.biz')
+# users.create!(name: 'David', surname: 'Boutin', title: 'Engineer', email: 'david.boutin@codeboxx.biz')
+# users.create!(name: 'Mathieu', surname: 'Lortie', title: 'Engineer', email: 'mathieu.lortie@codeboxx.biz')
+# users.create!(name: 'Thomas', surname: 'Carrier', title: 'Engineer', email: 'thomas.carrier@codeboxx.biz')
