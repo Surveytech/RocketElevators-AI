0cd8e104d3e11faa7819320aaeb1d731c6cd0cd2
CoteTommy <nordik.tommy@gmail.com>
2021-02-17 10:43:03 -0500

    Change quote column names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_205119) do
+ActiveRecord::Schema.define(version: 2021_02_17_153022) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -24,22 +24,22 @@ ActiveRecord::Schema.define(version: 2021_02_16_205119) do
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "type"
-    t.integer "apartments"
-    t.integer "floors"
-    t.integer "basements"
-    t.integer "companies"
-    t.integer "parkings"
-    t.integer "elevators"
-    t.integer "corporation"
-    t.integer "occupants"
+    t.string "building_type"
+    t.integer "number_of_apartments"
+    t.integer "number_of_floors"
+    t.integer "number_of_basements"
+    t.integer "number_of_companies"
+    t.integer "number_of_parking_spots"
+    t.integer "number_of_elevators"
+    t.integer "number_of_corporations"
+    t.integer "maximum_occupancy"
     t.string "product_line"
     t.float "elevator_unit_price"
-    t.float "elevator_total_cost"
-    t.float "installation_cost"
-    t.float "total_cost"
+    t.float "elevator_total_price"
+    t.float "installation_price"
+    t.float "total_price"
     t.string "company_name"
-    t.time "hours"
+    t.time "business_hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_quotes_on_name"
