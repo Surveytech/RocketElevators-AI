d5c24bfc9f31cb0b61ac32423fa4e57b9fa883f1
danigrum <danigrum@gmail.com>
2021-02-18 16:33:20 -0500

    :)

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "DanielaGrumbachKinder"
-set :repo_url, "https://github.com/danigrum/Rocket_Elevators_Information_System"
+set :repo_url, "https://github.com/danigrum/Rocket_Elevators_Information_System.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
