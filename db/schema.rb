f37828476a1f1a825c671f44483669b2980e9fc9
CoteTommy <nordik.tommy@gmail.com>
2021-02-17 21:43:38 -0500

    linked employees to user and created users for them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_170953) do
+ActiveRecord::Schema.define(version: 2021_02_18_010700) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_170953) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
     t.index ["first_name"], name: "index_employees_on_first_name"
   end
 
@@ -66,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_170953) do
     t.string "title"
     t.boolean "admin", default: false
     t.string "role_id", default: "User"
+    t.string "employee_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
