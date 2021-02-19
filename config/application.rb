467165b11b0fbaeca0f535c9727859840a66e327
riouxg <gabriel.rioux@hotmail.com>
2021-02-19 12:04:19 -0500

    rdy for test

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module RocketElevators
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = "testtest"
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
