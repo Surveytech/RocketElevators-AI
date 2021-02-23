3f88cfb35fc559f44ee5fddc976d33e69a96d2bf
CoteTommy <nordik.tommy@gmail.com>
2021-02-23 17:31:48 -0500

    Changed the input type and fixed the controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,29 +1,22 @@
 class LeadsController < ApplicationController
-  before_action :set_lead, only: %i[ show edit update destroy ]
 
-  # GET /leads or /leads.json
   def index
     @leads = Lead.all
   end
 
-  # GET /leads/1 or /leads/1.json
   def show
   end
 
-  # GET /leads/new
   def new
     @lead = Lead.new
   end
 
-  # GET /leads/1/edit
   def edit
   end
 
-  # POST /leads or /leads.json
-  def create
-    #puts params
 
-    file = lead_params.delete(:file)
+  def create
+    file = lead_params[:file]
 
     filename = file.original_filename
     filedata = file.read
@@ -36,18 +29,7 @@ class LeadsController < ApplicationController
 
   end
 
-  # PATCH/PUT /leads/1 or /leads/1.json
-  def update
-    respond_to do |format|
-      if @lead.update(lead_params)
-        format.html { redirect_to @lead, notice: "Lead was successfully updated." }
-        format.json { render :show, status: :ok, location: @lead }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @lead.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+
 
   # DELETE /leads/1 or /leads/1.json
   def destroy
