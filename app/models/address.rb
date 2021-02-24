738990fafe9a8ee45ef7a0c2b9175a189fdd5d2f
MalamutLeMit <eglapointe@hotmail.com>
2021-02-24 16:02:25 -0500

    Updated files from Patrick meeting, 24.02.2021

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+    def formatted_address
+        "#{self.address_street_name} #{self.address_city}"
+    end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ApplicationRecord
     has_one :users
+    has_many :building
 end
