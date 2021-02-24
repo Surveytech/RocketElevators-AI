1693b9e2b0dceb430e2747c382e434118ffd1fc7
CoteTommy <nordik.tommy@gmail.com>
2021-02-24 16:21:51 -0500

    added email field to quote table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_201202) do
+ActiveRecord::Schema.define(version: 2021_02_24_211842) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_type"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_201202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "email"
     t.index ["name"], name: "index_quotes_on_name"
     t.index ["user_id"], name: "index_quotes_on_user_id"
   end
