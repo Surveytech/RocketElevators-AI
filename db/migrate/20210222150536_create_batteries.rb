3448d1545ab5f99b7b6cd4653b5f281d4fc473f8
CoteTommy <nordik.tommy@gmail.com>
2021-02-24 17:24:53 -0500

    trying to fix to deploy

--- db/migrate/20210222150536_create_batteries.rb
+++ db/migrate/20210222150536_create_batteries.rb
@@ -5,7 +5,7 @@ class CreateBatteries < ActiveRecord::Migration[5.2]
       t.string :type, null: false
       t.string :status, null: false
       t.bigint :employee_id, null: false
-      t.datetime :date_of_commissioning, null: false
+      t.datetime :date_of_comissioning, null: false
       t.datetime :date_of_last_inspection, null: false
       t.string :certificate_of_operations, null: false
       t.string :information, null: false
