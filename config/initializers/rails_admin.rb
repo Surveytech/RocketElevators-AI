c9dfc9b9d0aa9bedb46c479132d20a56cf528497
CoteTommy <nordik.tommy@gmail.com>
2021-02-26 14:52:07 -0500

    gitignore, blazer fix,

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -18,12 +18,6 @@ RailsAdmin.config do |config|
   }
 
 
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
-
   config.actions do
     dashboard
     index
@@ -35,8 +29,5 @@ RailsAdmin.config do |config|
     delete
     show_in_app
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
