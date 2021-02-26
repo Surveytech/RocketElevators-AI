8555ef3ab4592068ea61dd57fbff79a230f6d849
riouxg <gabriel.rioux@hotmail.com>
2021-02-26 16:14:06 -0500

    after scrum

--- lib/pgsync.rb
+++ lib/pgsync.rb
@@ -63,32 +63,15 @@ class Pgsync
         end
     end
 
-<<<<<<< HEAD
     def sync_elevator
         Elevator.all.each do | elevator |
             puts "#{elevator}"
             sql = "INSERT INTO fact_elevator(serial_number, date_of_commissioning, building_id, customer_id, building_city)
             VALUES (#{elevator.id},'#{elevator.date_of_commissioning}',#{elevator.column_id},#{elevator.column.battery.building.customer_id},'#{elevator.column.battery.building.customer.address.city}');"
-=======
-    # def sync_elevators
-    #     Elevator.all.each do | elevator |
-    #         puts "#{elevator}"
-    #         sql = "INSERT INTO fact_elevator(serial_number, date_of_commissioning, building_id, customer_id, building_city)
-    #         VALUES (#{elevator.id},'#{elevator.date_of_commissioning}',#{elevator.column_id},#{elevator.column.battery.building.customer_id},'#{elevator.column.battery.building.customer.address.city}');"
-    #         self.conn.exec(sql)
-    #     end
-    # end
-
-    def sync_contact
-        Lead.all.each do | lead |
-            sql = "INSERT INTO fact_contact(contact_id, creation_date, company_name, email, project_name) 
-            VALUES (#{lead.id},'#{lead.created_at}','#{lead.company_name.gsub!(/[^0-9A-Za-z]/, '')}','#{lead.email}','#{lead.project_name}');"
->>>>>>> main
             self.conn.exec(sql)
         end
     end
 
-<<<<<<< HEAD
     def sync_contact
         Lead.all.each do | lead |
             sql = "INSERT INTO fact_contact(contact_id, creation_date, company_name, email, project_name) 
@@ -96,15 +79,6 @@ class Pgsync
             self.conn.exec(sql)
         end
     end
-=======
-    # def sync_customers
-    #     Customer.all.each do | customer |
-    #         sql = "INSERT INTO dim_customers(created_at, company_name, service_technical_authority_full_name, service_technical_authority_email, nbelevators, customer_city) 
-    #         VALUES ('#{customer.created_at}','#{customer.company_name}','#{customer.service_technical_authority_full_name}','#{customer.service_technical_authority_email}','#{quote.number_of_elevator}','#{customer.city}');"
-    #         self.conn.exec(sql)
-    #     end
-    # end 
->>>>>>> main
 
     def sync_customers
         Customer.all.each do | customer |
