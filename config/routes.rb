f4cb134265db5283a2582d00ca4c6ea2d3a7e106
CoteTommy <nordik.tommy@gmail.com>
2021-02-26 11:55:55 -0500

    added blazer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :leads
   resources :quotes
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  authenticate :user, ->(user) { user.admin? || user.employee? } do
+    mount Blazer::Engine, at: "blazer"
+  end
+  
   devise_for :users
 
   scope '/admin' do
