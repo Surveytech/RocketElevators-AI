0c219d7b89b8d0441c2c7425155d09187f4d02a3
riouxg <gabriel.rioux@hotmail.com>
2021-02-26 09:54:03 -0500

    2nd phase test syncing

--- lib/pgsync.rb
+++ lib/pgsync.rb
@@ -8,29 +8,26 @@ class Pgsync
     attr_accessor :conn, :host, :port, :database, :user, :password 
     
     def initialize
-        puts "init debut"
+        # puts "init debut"
         self.info
         self.connection 
-        # self.sync_quotes 
-        puts "init fin"
+        # puts "init fin"
     end
 
     def info
-        puts "debut info"
+        # puts "debut info"
         info = YAML.load(File.read("./config/pg.yml")) 
-        puts info
+        # puts info
         self.host = info["development"]["host"]
-        puts "host #{self.host}"
+        # puts "host #{self.host}"
         self.database = info["development"]["database"]
-        puts "database #{self.database}"
+        # puts "database #{self.database}"
         self.user = info["development"]["username"]
-        puts "user #{self.user}"
+        # puts "user #{self.user}"
         self.password = info["development"]["password"]
-        puts "password #{self.password}"
+        # puts "password #{self.password}"
         self.port = info["development"]["port"]
-        puts "port #{self.port}"
-
-        puts "info fin"
+        # puts "port #{self.port}"
     end
 
     def connection
@@ -41,50 +38,46 @@ class Pgsync
             user: self.user,
             password: self.password
         )    
-        puts "connection"    
+        # puts "connection"    
     end
 
 
-    def select
-        sql = "SELECT * FROM fact_quotes;" 
-        self.conn.exec(sql)
-    end
-
-    def sync_quotes
-        Quote.all.each do | quote |
-            puts "quote = #{quote}"
-            # sql = "INSERT INTO fact_quotes(id, created_at, company_name, email, number_of_elevator) 
-            # VALUES (#{quote.id},#{quote.created_at},#{quote.company_name},#{quote.number_of_elevator},#{customer.company_email})"
-            # self.conn.exec(sql)
-        end
-    end
+    # def sync_quotes
+    #     Quote.all.each do | quote |
+    #         # puts "#{quote}"
+    #         sql = "INSERT INTO fact_quotes(quote_id, creation_date, company_name, email) 
+    #         VALUES (#{quote.id},'#{quote.created_at}','#{quote.company_name}','#{quote.email}');"
+    #         self.conn.exec(sql)
+    #     end
+    # end
 
     # def sync_elevators
     #     Elevator.all.each do | elevator |
     #         sql = "INSERT INTO fact_elevator (serial_number, date_of_commissioning, building_id, customer_id, building_city) 
-    #         VALUES (#{elevator.id},#{elevator.date_of_commissioning},#{building.id},#{customer.id},#{adress.city})"
-    #         self.conn.econnectionxec(sql)
+    #         VALUES (#{elevator.id},'#{elevator.date_of_commissioning}',#{building.id},#{customer.id},#{adress.city});",
+    #         self.conn.exec(sql)
     #     end
     # end
 
     # def sync_contact
     #     Lead.all.each do | lead |
-    #         sql = "INSERT INTO fact_contact(id, created_at, company_name, email, project_name) 
-    #         VALUES (#{lead.id},#{lead.created_at},#{lead.company_name},#{lead.email}.#{lead.project_name})"
+    #         sql = "INSERT INTO fact_contact(contact_id, creation_date, company_name, email, project_name) 
+    #         VALUES (#{lead.id},'#{lead.created_at}','#{lead.company_name.gsub!(/[^0-9A-Za-z]/, '')}','#{lead.email}','#{lead.project_name}');"
     #         self.conn.exec(sql)
     #     end
     # end
 
-    # def sync_customers
-    #     Customer.all.each do | customer |
-    #         sql = "INSERT INTO dim_customers(created_at, company_name, service_technical_authority_full_name, service_technical_authority_email, nbelevators, customer_city) 
-    #         VALUES (#{customer.created_at},#{customer.company_name},#{customer.service_technical_authority_full_name},#{customer.service_technical_authority_email},#{quote.number_of_elevator},#{adress.city})"
-    #         self.conn.exec(sql)
-    #     end
-    # end 
+    def sync_customers
+        Customer.all.each do | customer |
+            sql = "INSERT INTO dim_customers(created_at, company_name, service_technical_authority_full_name, service_technical_authority_email, nbelevators, customer_city) 
+            VALUES ('#{customer.created_at}','#{customer.company_name}','#{customer.service_technical_authority_full_name}','#{customer.service_technical_authority_email}','#{quote.number_of_elevator}','#{customer.city}');"
+            self.conn.exec(sql)
+        end
+    end 
 
     # Pgsync.sync_quotes()
     # Pgsync.sync_contact()
     # Pgsync.sync_elevators()
-#     Pgsync.sync_customers()
+    # Pgsync.sync_customers()
 end
+
