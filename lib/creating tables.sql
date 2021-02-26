6d4b0eea7400d19578da14a1749b01eb7a09a837
riouxg <gabriel.rioux@hotmail.com>
2021-02-26 14:49:54 -0500

    creates table and requests

--- lib/creating tables.sql
+++ lib/creating tables.sql
@@ -1,30 +1,29 @@
 CREATE TABLE fact_contact (
-    contact_id INTEGER PRIMARY KEY, 
+    contact_id serial PRIMARY KEY, 
     creation_date TIMESTAMP NOT NULL, 
     company_name VARCHAR (50), 
     email VARCHAR (255) NOT NULL, 
-    project_name VARCHAR (50) NOT NULL
+    project_name VARCHAR (50)
     );
-CREATE TABLE FactElevator (
-    serial_number INTEGER PRIMARY KEY, 
-    date_of_commissioning timestamp NOT NULL, 
-    building_id INTEGER NOT NULL, 
-    customer_id INTEGER NOT NULL, 
-    building_city VARCHAR(50) NOT NULL
+CREATE TABLE fact_elevator (
+    serialnumber serial PRIMARY KEY, 
+    date_of_commissioning timestamp, 
+    building_id serial, 
+    customer_id serial, 
+    building_city VARCHAR(50)
     );
 CREATE TABLE dim_customers (
     creation_date timestamp PRIMARY KEY, 
     company_name VARCHAR (255) NOT NULL, 
-    first_name_company_main_contact VARCHAR(50) NOT NULL, 
-    last_name_company_main_contact VARCHAR(50) NOT NULL, 
+    full_name_company_main_contact VARCHAR(50) NOT NULL, 
     email_of_company_main_contact VARCHAR(255) NOT NULL, 
-    nb_elevators INTEGER NOT NULL, 
-    customers_city VARCHAR(100) NOT NULL   
+    nbelevators smallint, 
+    customers_city VARCHAR(100)    
     );
 CREATE TABLE fact_quotes (
-    quote_id serial PRIMARY KEYs, 
+    quote_id serial PRIMARY KEY, 
     creation_date TIMESTAMP NOT NULL, 
-    company_name VARCHAR (50) NOT NULL, 
+    company_name VARCHAR (50), 
     email VARCHAR ( 255 ) NOT NULL, 
-    nb_elevators INTEGER NOT NULL
+    nb_elevator SMALLINT
     );
