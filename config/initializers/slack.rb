c6d19d2e4d1cfaedae7ac883fa9d4ea456e5ba4f
DGA-Charles <charles.e.hall@hotmail.com>
2021-03-09 14:58:26 -0500

    hid the webhook url

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -9,7 +9,7 @@ class Slack
     end
 
     def sendMsg(elevator)
-        url = 'https://hooks.slack.com/services/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'
+        url = ENV['SLACK_URL']
         msg = "The Elevator #{elevator.id} with Serial Number #{elevator.serial_number} changed status from Active to Inactive"
         HTTParty.post(url, body: {"text":"#{msg}"}.to_json)
     end
