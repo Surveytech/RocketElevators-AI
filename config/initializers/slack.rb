fcb02f8b07c18e646fe7e1a1c588619d7c14c46e
DGA-Charles <charles.e.hall@hotmail.com>
2021-03-09 13:51:26 -0500

    merging

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -8,10 +8,10 @@ class Slack
        sendMsg(elevator)
     end
 
-    # def sendMsg(elevator)
-    #     url = 'https://hooks.slack.com/services/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'
-    #     msg = "The Elevator #{elevator.id} with Serial Number #{elevator.serial} changed status from #{[Old Status]} to #{[New Status]}"
-    #     HTTParty.post(url, body: {"text":"#{msg}"}.to_json)
-    # end
+    def sendMsg(elevator)
+        url = 'https://hooks.slack.com/services/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'
+        msg = "The Elevator #{elevator.id} with Serial Number #{elevator.serial_number} changed status from Active to Inactive"
+        HTTParty.post(url, body: {"text":"#{msg}"}.to_json)
+    end
 
 end
