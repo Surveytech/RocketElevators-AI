a1ac8811018582ab15ff7edf49a699e81b24b062
DGA-Charles <charles.e.hall@hotmail.com>
2021-03-10 11:05:24 -0500

    Finished the Slack bot... finally

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,3 +1,25 @@
+require 'httparty'
+
 class Elevator < ApplicationRecord
+    include ActiveModel::Dirty
     belongs_to :column
+    
+    
+    before_update :sendMsg
+    
+    def getElevator
+        return "Elevator(self.id)"
+    end
+    
+    private
+    def sendMsg
+        if self.status_changed?
+            
+            
+            url = ENV['SLACK_URL']
+            msg = "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_was} to #{self.status}"
+            HTTParty.post(url, body: {"text":"#{msg}"}.to_json)
+        end 
+        
+    end
 end
