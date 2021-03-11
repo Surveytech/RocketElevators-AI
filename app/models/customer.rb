65607752e13f2c4decb973609065c1220bc0ac6e
Felix-Masse <flxmasse@hotmail.com>
2021-03-11 10:37:40 -0500

    Dropbox automation and exeption

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
+require 'dropbox'
+
 class Customer < ApplicationRecord
+    
     has_one :user
     has_one :address
     has_many :building
@@ -9,15 +12,25 @@ class Customer < ApplicationRecord
 
     def dropbox_file
 
-        #dbx = Dropbox::Client.new('FhuKAHEcOXsAAAAAAAAAAcst4lCatrvUE1ZDplH3l6Z7EVSoqT1vfGyCoMrURZjR')
-            
-        begin
-            folder = dbx.create_folder("/#{self.company_email}") # => Dropbox::FolderMetadata
-        rescue => exception
-            
-        end
+        dbx = Dropbox::Client.new(ENV['DROPBOX_TOKEN'])
+        num = 1
+        path= self.company_email
 
-        file = dbx.upload("/#{self.company_email}/file1.txt", "file body") # => Dropbox::FileMetadata
+        Lead.all.each do |lead|
+            if lead.email == self.company_email
+                begin
+                    file = dbx.upload("/#{path}/#{lead.file_name}", "#{lead.file_data}") # => Dropbox::FileMetadata
+                rescue => exception
+                    num += 1
+                    name = lead.file_name.split('.')
+                    new_name = name[0] + num.to_s + '.' +name[1]
+                    file = dbx.upload("/#{path}/#{new_name}", "#{lead.file_data}") # => Dropbox::FileMetadata
+                    Lead.where(:id => lead.id).update(:file_name => new_name)
+                end
+                Lead.where(:id => lead.id).update(:file_data => nil)
+            end
+        end
+        
 
     end
 
