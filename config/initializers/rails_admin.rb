da2f46075458aace9c5b09423b0053fc71c53ec9
CoteTommy <nordik.tommy@gmail.com>
2021-03-11 17:34:19 -0500

    Dynamic watson

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -17,7 +17,6 @@ RailsAdmin.config do |config|
   config.navigation_static_links = {
     'Home' => '/',
     'Blazer' => '/blazer',
-    'Charts' => '/blazer/dashboards/1-main-dashboard',
   }
 
   config.actions do
