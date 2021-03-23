594c996422e1720bac61f68745f8505d4336c05c
CoteTommy <nordik.tommy@gmail.com>
2021-03-23 12:23:29 -0400

    Changed blob to medium blob for file upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_174809) do
+ActiveRecord::Schema.define(version: 2021_03_23_162104) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_type"
@@ -207,7 +207,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_174809) do
     t.string "message", null: false
     t.string "file_name"
     t.string "file_type"
-    t.binary "file_data"
+    t.binary "file_data", limit: 16777215
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
