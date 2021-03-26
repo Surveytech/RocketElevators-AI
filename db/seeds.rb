7f389bc098228e84334c2d191de46e185980c031
CoteTommy <nordik.tommy@gmail.com>
2021-03-26 14:39:35 -0400

    removed empty users from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,6 @@ r1 = Role.create({ name: 'Regular', description: 'Can read and create quotes. Ca
 r2 = Role.create({ name: 'Employee', description: 'Employee' })
 r3 = Role.create({ name: 'Admin', description: 'Can perform any CRUD operation on any resource' })
 
-u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id })
-u2 = User.create({ name: 'Sue', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id })
-u3 = User.create({ name: 'Kev', email: 'kev@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
-u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r3.id })
-u5 = User.create({ name: 'Admin', email: 'admin@admin', password: '123456', password_confirmation: '123456', role_id: r3.id })
-
 Employee.all.each do |employee|
   employee.build_user({
     email: employee.email,
@@ -33,6 +27,7 @@ Employee.all.each do |employee|
   employee.save!
  end
 
+User.create({ name: 'Admin', email: 'admin@admin', password: '123456', password_confirmation: '123456', role_id: r3.id })
 $addressArray = []
 
 
